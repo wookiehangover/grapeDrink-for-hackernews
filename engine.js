df51a73232593875d485a48821ce4dee3d273163
Sam Breed <sam@quickleft.com>
2010-12-21 22:17:06 -0700

    added comment highlighting

--- engine.js
+++ engine.js
@@ -1,8 +1,61 @@
 (function($){
+
+$('center > table').attr('bgcolor', '#eaeaea');
+$('img[width="18"]').css('opacity', '0.5');
+$('td[bgcolor="#ff6600"]').attr('bgcolor', '#ff00ff');
+
+var karma = $('td[bgcolor="#ff00ff"] td:last-child > span'),
+    over9000 = function(anything){
+      var nineThousand = anything.html().replace(/\(+.+\)/, "(9001)");
+
+      anything.html(nineThousand);
+    };
+
+over9000(karma);
+
+
 $('a').click(function(e){
+  cornify_add();
+
 	if(this.href.indexOf('news.ycombinator') < 0) {
 		e.preventDefault();
 		window.open(this.href);
-	} 
+	}
 });
+
+var comments = $('.subtext  a:last-child'),
+    comment_count = [];
+
+comments.css({ 'padding': '0 2px 2px 2px'});
+
+comments.each(function(i, elem){
+  var text = $(elem).text();
+
+  text = text.replace(/^discuss$/, "0");
+  text = text.replace(/.comments$/, "");
+
+  var c = {
+    'jq': $(elem),
+    'count': parseInt(text)
+  }
+
+  comment_count.push(c);
+});
+
+comment_count.sort(function(a, b) { return a.count - b.count });
+
+var f = 0;
+var i = comment_count.length;
+
+var j, scale = 200 / i;
+while(i--){
+  j = ~~(f*scale);
+  if(comment_count[i]["count"] > 75 ) { 
+    comment_count[i]["jq"].css({'background': "rgb(255, 0, 0)", color: "#fff"});
+  } else {
+    comment_count[i]["jq"].css({'background': "rgb("+ j +", "+ j +", "+ j +")", color: "#fff"});
+  }
+  f++;
+}
+
 })(jQuery);
